9467a08bf7b2261105463ed4b59683672bc49837
Zhengri Fan <zf002@bucknell.edu>
2015-04-16 17:36:34 -0400

    Warning msg

--- src/Main/MainApp.java
+++ src/Main/MainApp.java
@@ -47,6 +47,9 @@ public class MainApp extends Application {
         this.primaryStage.setTitle("Texas Hold'em");
 
         FXMLLoader loader = new FXMLLoader();
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //For the program to run, please ues abslute path here!!!!
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         File xmlFile = new File("C:/CS/csci205_finalproject/src/view/gameView.fxml");
         loader.setLocation(xmlFile.toURI().toURL());
         root = (AnchorPane) loader.load();
