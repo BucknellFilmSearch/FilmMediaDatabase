3cb2713cd3c2cdec13480a0100124d02dd268e01
Zhengri Fan <zf002@bucknell.edu>
2015-04-16 16:05:22 -0400

    GUI part1 finished

---
+++ src/Controller/FXMLController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Zhengri Fan
+ */
+public class FXMLController implements Initializable {
+
+    @FXML
+    private HBox bscBox;
+
+    @FXML
+    private ImageView tableImage;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
