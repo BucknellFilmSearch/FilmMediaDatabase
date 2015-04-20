71f9194122e14c459b0473c334e18aa8af4734f6
Zhengri Fan <zf002@bucknell.edu>
2015-04-20 10:51:43 -0400

    usetext

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -22,6 +22,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.Bloom;
@@ -98,7 +99,19 @@ public class MainController implements Initializable {
     private Group raiseGroup;
 
     @FXML
-    private Button btnStep;
+    private HBox cardsAfterWinning;
+
+    @FXML
+    private Label textPlayer1;
+
+    @FXML
+    private Label textPlayer2;
+
+    @FXML
+    private Label textPlayer3;
+
+    @FXML
+    private Label textPlayer4;
     // </editor-fold>
 
     @FXML
