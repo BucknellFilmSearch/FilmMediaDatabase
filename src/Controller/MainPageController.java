e4feab9853c6f89fb64f337f83d2e8632e551b5f
Zhengri Fan <zf002@bucknell.edu>
2015-04-21 23:17:04 -0400

    commented bug code

--- src/Controller/MainPageController.java
+++ src/Controller/MainPageController.java
@@ -7,20 +7,10 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.animation.Interpolator;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
-import javafx.util.Duration;
 import view.MultiPaneHolder;
 
 /**
@@ -48,34 +38,34 @@ public class MainPageController implements Initializable {
 
     @FXML
     private void startGame() {
-        // Create snapshots with the last state of the scenes
-        WritableImage wi = new WritableImage(1280, 720);
-        Image img1 = root.getCurPane().snapshot(new SnapshotParameters(), wi);
-        ImageView imgView1 = new ImageView(img1);
-        wi = new WritableImage(1280, 720);
-        Image img2 = root.getPane(MultiPaneHolder.GamePane.GameScreen).snapshot(new SnapshotParameters(), wi);
-        ImageView imgView2 = new ImageView(img2);
-        // Create new pane with both images
-        imgView1.setTranslateX(0);
-        imgView2.setTranslateX(300);
-        StackPane pane = new StackPane(imgView1, imgView2);
-        pane.setPrefSize(300, 250);
-        AnchorPane oldPane = (AnchorPane) root.getCurPane();
-        // Replace root1 with new pane
-        ((AnchorPane) root.getCurPane()).getChildren().setAll(pane);
-        // create transtition
-        Timeline timeline = new Timeline();
-        KeyValue kv = new KeyValue(imgView2.translateXProperty(), 0, Interpolator.EASE_BOTH);
-        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
-        timeline.getKeyFrames().add(kf);
-        timeline.setOnFinished(t -> {
-            // remove pane and restore scene 1
-            ((AnchorPane) root.getCurPane()).getChildren().setAll(oldPane);
-            // set scene 2
-            //primaryStage.setScene(scene2);
-//            root.setDisplayPane(MultiPaneHolder.GamePane.GameScreen);
-        });
-        timeline.play();
+//        // Create snapshots with the last state of the scenes
+//        WritableImage wi = new WritableImage(1280, 720);
+//        Image img1 = root.getCurPane().snapshot(new SnapshotParameters(), wi);
+//        ImageView imgView1 = new ImageView(img1);
+//        wi = new WritableImage(1280, 720);
+//        Image img2 = root.getPane(MultiPaneHolder.GamePane.GameScreen).snapshot(new SnapshotParameters(), wi);
+//        ImageView imgView2 = new ImageView(img2);
+//        // Create new pane with both images
+//        imgView1.setTranslateX(0);
+//        imgView2.setTranslateX(300);
+//        StackPane pane = new StackPane(imgView1, imgView2);
+//        pane.setPrefSize(300, 250);
+//        AnchorPane oldPane = (AnchorPane) root.getCurPane();
+//        // Replace root1 with new pane
+//        ((AnchorPane) root.getCurPane()).getChildren().setAll(pane);
+//        // create transtition
+//        Timeline timeline = new Timeline();
+//        KeyValue kv = new KeyValue(imgView2.translateXProperty(), 0, Interpolator.EASE_BOTH);
+//        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+//        timeline.getKeyFrames().add(kf);
+//        timeline.setOnFinished(t -> {
+//            // remove pane and restore scene 1
+//            ((AnchorPane) root.getCurPane()).getChildren().setAll(oldPane);
+//            // set scene 2
+//            //primaryStage.setScene(scene2);
+////            root.setDisplayPane(MultiPaneHolder.GamePane.GameScreen);
+//        });
+//        timeline.play();
 
         root.setDisplayPane(MultiPaneHolder.GamePane.GameScreen);
 
