cc6ce7c0dd7b96a369835c4e10ff1668a6854ea1
Zhengri Fan <zf002@bucknell.edu>
2015-04-24 00:07:08 -0400

    Done for now

--- src/Controller/MainPageController.java
+++ src/Controller/MainPageController.java
@@ -11,6 +11,7 @@ import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -103,6 +104,33 @@ public class MainPageController implements Initializable {
         timeline.play();
     }
 
+    @FXML
+    private void exitApp() {
+        Platform.exit();
+    }
+
+//    @FXML
+//    private void seeOption(){
+//        WritableImage wi = new WritableImage(1280, 720);
+//        Image img1 = root.getCurPane().snapshot(new SnapshotParameters(), wi);
+//        ImageView imgView1 = new ImageView(img1);
+//        wi = new WritableImage(1280, 720);
+//        Image img2 = root.getPane(MultiPaneHolder.GamePane.HelpView).snapshot(new SnapshotParameters(), wi);
+//        ImageView imgView2 = new ImageView(img2);
+//        imgView1.setTranslateX(0);
+//        imgView2.setTranslateY(720);
+//        ((StackPane) root.getPane(MultiPaneHolder.GamePane.TransitionGroup)).getChildren().add(imgView1);
+//        ((StackPane) root.getPane(MultiPaneHolder.GamePane.TransitionGroup)).getChildren().add(imgView2);
+//        root.setDisplayPane(MultiPaneHolder.GamePane.TransitionGroup);
+//        Timeline timeline = new Timeline();
+//        KeyValue kv = new KeyValue(imgView2.translateYProperty(), 0, Interpolator.EASE_BOTH);
+//        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+//        timeline.getKeyFrames().add(kf);
+//        timeline.setOnFinished(t -> {
+//            root.setDisplayPane(MultiPaneHolder.GamePane.HelpView);
+//        });
+//        timeline.play();
+//    }
     public static void setRoot(MultiPaneHolder root) {
         MainPageController.root = root;
     }
