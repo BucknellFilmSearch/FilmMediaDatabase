a0e5e07053ea60fe4129bb9b8263082e062c6fa6
Justin Eyster <jde012@bucknell.edu>
2015-05-21 12:57:52 -0400

    Wrote Movie database entry method, to use for data entry purposes.

--- src/CPCPModel/dbDataEntry.py
+++ src/CPCPModel/dbDataEntry.py
@@ -16,12 +16,40 @@ class dbDataEntry():
         
     def mainMenu(self):
         while True:
-            choice = input("Enter 0 to enter a movie into the database, 1 for a TV show, or 2 to exit.")
-            if choice == 0:
+            choice = input("Enter 0 to enter a movie into the database, 1 for a TV show, or 2 to exit: ")
+            if choice == "0":
                 self.enterMovie()
-            elif choice == 1:
+            elif choice == "1":
                 self.enterTVShow()
-            elif choice == 2:
+            elif choice == "2":
                 sys.exit()
             else:
                 print("Invalid entry. Try again.")
+
+    def enterMovie(self):
+        OCLC_ID = input("Enter the DVD's unique OCLC number (from Bucknell's WorldCat catalog): ")
+        Title = input("Movie title: ")
+        Director = input("Director: ")
+        MovieReleaseYear = input("Movie release year: ")
+        DVDReleaseYear = input("DVD release year: ")
+        Country1 = input("Country 1: ")
+        Country2 = input("Country 2 (if applicable): ")
+        Country3 = input("Country 3 (if applicable): ")
+        Genre1 = input("Genre 1: ")
+        Genre2 = input("Genre 2 (if applicable): ")
+        Genre3 = input("Genre 3 (if applicable): ")
+        MPAARating = input("MPAA rating: ")
+        RuntimeInMinutes = input("Run time (in minutes): ")
+        CCorSub = input("CC or Sub: ")
+        
+        with self.connection:
+            
+            cursor = self.connection.cursor()
+            cursor.execute("INSERT INTO MOVIES VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", \
+            (OCLC_ID, Title, Director, MovieReleaseYear, DVDReleaseYear, Country1, Country2, \
+            Country3, Genre1, Genre2, Genre3, MPAARating, RuntimeInMinutes, CCorSub))
+    
+    def enterTVShow(self):
+        pass
+    
+dataEntry = dbDataEntry()
