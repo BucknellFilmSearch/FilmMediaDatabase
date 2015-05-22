9a48360e46951cb2293a6c85b0b61919262640a6
Justin Eyster <jde012@bucknell.edu>
2015-05-22 10:41:02 -0400

    Wrote method for entering TV shows into the database. Added more documentation.

--- src/CPCPModel/dbDataEntry.py
+++ src/CPCPModel/dbDataEntry.py
@@ -9,12 +9,18 @@ import sqlite3 as lite
 import sys
 
 class dbDataEntry():
+    """ Class built for the purpose of data entry. This class will allow a
+    member of our team to enter the metadata for movies in our database.
+    When complete, it will also automatically fill the tables which store
+    the CC/subtitle text. """
     
     def __init__(self):
+        # create connection to database and then go to main menu
         self.connection = lite.connect('cpcp.db')
         self.mainMenu()
         
     def mainMenu(self):
+        """ Main menu for data entry. """
         while True:
             choice = input("Enter 0 to enter a movie into the database, 1 for a TV show, or 2 to exit: ")
             if choice == "0":
@@ -48,20 +54,56 @@ class dbDataEntry():
             cursor.execute("INSERT INTO MOVIES VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", \
             (OCLC_ID, Title, Director, MovieReleaseYear, DVDReleaseYear, Country1, Country2, \
             Country3, Genre1, Genre2, Genre3, MPAARating, RuntimeInMinutes, CCorSub))
+            
+        # create the table to hold this movie's text
+        self.createMediaTextTable(OCLC_ID)
     
     def enterTVShow(self):
-        pass
-    
-    def createMovieLinesTable(self, oclcId):
-        """ This method creates a table to store the text of an individual movie. """
+        OCLC_ID = input("Enter the DVD's unique OCLC number (from Bucknell's WorldCat catalog): ")
+        ShowTitle = input("Show title: ")
+        EpisodeTitle = input("Episode title: ")
+        SeasonNumber = input("Season number: ")
+        EpisodeNumber = input("Episode number: ")
+        Director = input("Director: ")
+        EpisodeReleaseYear = input("Episode release year: ")
+        DVDReleaseYear = input("DVD release year: ")
+        Country1 = input("Country 1: ")
+        Country2 = input("Country 2 (if applicable): ")
+        Country3 = input("Country 3 (if applicable): ")
+        Genre1 = input("Genre 1: ")
+        Genre2 = input("Genre 2 (if applicable): ")
+        Genre3 = input("Genre 3 (if applicable): ")
+        TVRating = input("TV rating: ")
+        RuntimeInMinutes = input("Run time (in minutes): ")
+        CCorSub = input("CC or Sub: ")
         
         with self.connection:
             
             cursor = self.connection.cursor()
+            cursor.execute("INSERT INTO MOVIES VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", \
+            (OCLC_ID, ShowTitle, EpisodeTitle, SeasonNumber, EpisodeNumber, \
+            Director, EpisodeReleaseYear, DVDReleaseYear, Country1, Country2, \
+            Country3, Genre1, Genre2, Genre3, TVRating, RuntimeInMinutes, CCorSub))
+            
+        # create the table to hold this tv show's text
+        self.createMediaTextTable(OCLC_ID)
+    
+    def createMediaTextTable(self, oclcId):
+        """ This method creates a table to store the text of an individual movie
+        or TV Show, given the oclc number of the DVD. """
+        
+        with self.connection:
+            
+            cursor = self.connection.cursor()
+            # NOTE: concatenating the table name onto the SQL command in the manner
+            # below may leave the program vulnerable to SQL injection.
+            # Look for better way of doing this, for sake of online version.
+            # (This may not be a concern since it's in the database setup phase)
             cursor.execute("DROP TABLE IF EXISTS " + oclcId)
             cursor.execute("CREATE TABLE " + oclcId + " ( \
             LineNumber          VARCHAR            PRIMARY KEY, \
             StartTimeStamp      VARCHAR, \
-            EndTimeStamp        VARCHAR)")
+            EndTimeStamp        VARCHAR, \
+            LineText            VARCHAR)")
     
 dataEntry = dbDataEntry()
