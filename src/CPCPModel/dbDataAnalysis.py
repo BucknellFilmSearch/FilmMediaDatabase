b4d1112b97dc1a0d4b289bb781687b463c2a3568
Justin Eyster <jde012@bucknell.edu>
2015-05-29 12:31:47 -0400

    Wrote function to return count of keyword/phrase by MPAA rating. Also wrote version of count by year that counts each movie only once. This will allow us to find percentage of movies in a given year that contain the word/phrase.

--- src/CPCPModel/dbDataAnalysis.py
+++ src/CPCPModel/dbDataAnalysis.py
@@ -8,11 +8,23 @@ __date__ = "$May 29, 2015 9:26:40 AM$"
 import sqlite3 as lite
 import sys
 
-def occurrencesAcrossReleaseYears(keywordOrPhrase):
+def cumulativeOccurrencesAcrossReleaseYears(keywordOrPhrase):
     connection = lite.connect('cpcp.db')
     with connection:
         cursor = connection.cursor()
         cursor.execute("SELECT MOVIES.MovieReleaseYear, COUNT(*) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY MovieReleaseYear")
+        data = cursor.fetchall()
+        print(data)
+        return data
+    
+def occurrencesAcrossReleaseYears(keywordOrPhrase):
+    """ Same as above but counts each movie only once. """
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MOVIES.MovieReleaseYear, COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES, ALLTEXT \
         WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
         + keywordOrPhrase + "'" + " GROUP BY MovieReleaseYear")
         data = cursor.fetchall()
@@ -26,6 +38,17 @@ def occurrencesAcrossTimeStamps(keywordOrPhrase):
         cursor.execute("SELECT ALLTEXT.StartTimeStamp, COUNT(*) FROM MOVIES, ALLTEXT \
         WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
         + keywordOrPhrase + "'" + " GROUP BY StartTimeStamp")
+        data = cursor.fetchall()
+        print(data)
+        return data
+    
+def cumulativeOccurrencesByMPAARating(keywordOrPhrase):
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MOVIES.MPAARating, COUNT(*) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY MPAARating")
         data = cursor.fetchall()
         print(data)
         return data
