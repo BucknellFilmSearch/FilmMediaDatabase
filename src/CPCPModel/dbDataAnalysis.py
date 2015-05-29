1be3d7dd912c7640158426f5d15e32cdce1bfc12
Justin Eyster <jde012@bucknell.edu>
2015-05-29 10:30:45 -0400

    Began writing functions for meaningful analysis of results. Wrote function to return count of keyword by year, and a function to return timestamps of occurrences.

---
+++ src/CPCPModel/dbDataAnalysis.py
@@ -0,0 +1,34 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "justi_000"
+__date__ = "$May 29, 2015 9:26:40 AM$"
+
+import sqlite3 as lite
+import sys
+
+def occurrencesAcrossReleaseYears(keywordOrPhrase):
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MOVIES.MovieReleaseYear, COUNT(*) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY MovieReleaseYear")
+        data = cursor.fetchall()
+        print(data)
+        return data
+        
+def occurrencesAcrossTimeStamps(keywordOrPhrase):
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT ALLTEXT.StartTimeStamp, COUNT(*) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY StartTimeStamp")
+        data = cursor.fetchall()
+        print(data)
+        return data
+    
+occurrencesAcrossReleaseYears("cell phone")
+    
