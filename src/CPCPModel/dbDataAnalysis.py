f597c9b814266d286a47d5aac20258836c7a227e
Justin Eyster <jde012@bucknell.edu>
2015-06-01 11:43:12 -0400

    Wrote functions that calculate percentages of movies of certain year, genre, or rating that have the specified keyword/phrase. Some of these functions are more efficient than others, but they all work.

--- src/CPCPModel/dbDataAnalysis.py
+++ src/CPCPModel/dbDataAnalysis.py
@@ -23,6 +23,16 @@ def searchResults(keywordOrPhrase):
         + keywordOrPhrase + "'")
         data = cursor.fetchall()
         return data
+    
+def occurrencesByTimeStamps(keywordOrPhrase):
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT ALLTEXT.StartTimeStamp, COUNT(*) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY StartTimeStamp")
+        data = cursor.fetchall()
+        return data
 
 def cumulativeOccurrencesByReleaseYear(keywordOrPhrase):
     connection = lite.connect('cpcp.db')
@@ -35,7 +45,8 @@ def cumulativeOccurrencesByReleaseYear(keywordOrPhrase):
         return data
     
 def occurrencesByReleaseYear(keywordOrPhrase):
-    """ Same as above but counts each movie only once. """
+    """ Same as above but counts each movie only once. Useful for returning
+    percentage of movies containing keyword in certain year. """
     connection = lite.connect('cpcp.db')
     with connection:
         cursor = connection.cursor()
@@ -44,16 +55,25 @@ def occurrencesByReleaseYear(keywordOrPhrase):
         + keywordOrPhrase + "'" + " GROUP BY MovieReleaseYear")
         data = cursor.fetchall()
         return data
-        
-def occurrencesByTimeStamps(keywordOrPhrase):
+    
+def totalMoviesOfSpecifiedYear(year):
+    """ Helper function for percentageOfOccurrenceByReleaseYear. """
     connection = lite.connect('cpcp.db')
     with connection:
         cursor = connection.cursor()
-        cursor.execute("SELECT ALLTEXT.StartTimeStamp, COUNT(*) FROM MOVIES, ALLTEXT \
-        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
-        + keywordOrPhrase + "'" + " GROUP BY StartTimeStamp")
+        cursor.execute("SELECT COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES \
+        WHERE MOVIES.MovieReleaseYear = " + year)
         data = cursor.fetchall()
-        return data
+        return data[0][0]
+    
+def percentageOfOccurrenceByReleaseYear(keywordOrPhrase):
+    """ Returns the percentages of movies containing the keyword/phrase for each
+    movie release year that occurs in the database. """
+    counts = occurrencesByReleaseYear(keywordOrPhrase)
+    listOfPercentages = []
+    for count in counts:
+        listOfPercentages += [(count[0], 100 * count[1] / totalMoviesOfSpecifiedYear(count[0]))]
+    return listOfPercentages
     
 def cumulativeOccurrencesByMPAARating(keywordOrPhrase):
     connection = lite.connect('cpcp.db')
@@ -64,6 +84,37 @@ def cumulativeOccurrencesByMPAARating(keywordOrPhrase):
         + keywordOrPhrase + "'" + " GROUP BY MPAARating")
         data = cursor.fetchall()
         return data
+    
+def occurrencesByMPAARating(keywordOrPhrase):
+    """ Same as above but counts each movie only once. Useful for returning
+    percentage of movies containing keyword of certain rating. """
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MOVIES.MPAARating, COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY MPAARating")
+        data = cursor.fetchall()
+        return data
+    
+def totalMoviesOfSpecifiedMPAARating(rating):
+    """ Helper function for percentageOfOccurrenceByMPAARating. """
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES \
+        WHERE MOVIES.MPAARating = '" + rating + "'")
+        data = cursor.fetchall()
+        return data[0][0]
+    
+def percentageOfOccurrenceByMPAARating(keywordOrPhrase):
+    """ Returns the percentages of movies containing the keyword/phrase for each
+    movie MPAA rating that occurs in the database. """
+    counts = occurrencesByMPAARating(keywordOrPhrase)
+    listOfPercentages = []
+    for count in counts:
+        listOfPercentages += [(count[0], 100 * count[1] / totalMoviesOfSpecifiedMPAARating(count[0]))]
+    return listOfPercentages
     
 def cumulativeOccurrencesByGenre1(keywordOrPhrase):
     connection = lite.connect('cpcp.db')
@@ -120,5 +171,80 @@ def cumulativeOccurrencesByGenre(keywordOrPhrase, genre):
     
     return totalOccurrencesInGenre
 
+def occurrencesByGenre1(keywordOrPhrase):
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MOVIES.Genre1, COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY Genre1")
+        data = cursor.fetchall()
+        return data
     
-print(cumulativeOccurrencesByGenre("cell phone", "Thriller"))
+def occurrencesByGenre2(keywordOrPhrase):
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MOVIES.Genre2, COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY Genre2")
+        data = cursor.fetchall()
+        return data
+    
+def occurrencesByGenre3(keywordOrPhrase):
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT MOVIES.Genre3, COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES, ALLTEXT \
+        WHERE ALLTEXT.OCLC_ID = MOVIES.OCLC_ID AND ALLTEXT.LineText MATCH '" \
+        + keywordOrPhrase + "'" + " GROUP BY Genre3")
+        data = cursor.fetchall()
+        return data
+    
+def occurrencesByGenre(keywordOrPhrase, genre):
+    """ Function to return the total occurrences of a keyword/phrase in movies
+    of a specified genre. Less efficient than searching metadata categories
+    that only have one column. Has to query the database three times instead
+    of once, and then sum the results.
+    """
+    genre1Counts = occurrencesByGenre1(keywordOrPhrase)
+    genre2Counts = occurrencesByGenre2(keywordOrPhrase)
+    genre3Counts = occurrencesByGenre3(keywordOrPhrase)
+    totalOccurrencesInGenre = 0
+    # count occurrences of keyword in genre1, matching specified genre
+    for count1 in genre1Counts:
+        if count1[0] == genre:
+            totalOccurrencesInGenre += count1[1]
+    # count occurrences of keyword in genre2, matching specified genre
+    for count2 in genre2Counts:
+        if count2[0] == genre:
+            totalOccurrencesInGenre += count2[1]
+    # count occurrences of keyword in genre3, matching specified genre
+    for count3 in genre3Counts:
+        if count3[0] == genre:
+            totalOccurrencesInGenre += count3[1]
+    
+    return totalOccurrencesInGenre
+
+def totalMoviesOfSpecifiedGenre(genre):
+    """ Helper function for percentageOfOccurrenceByGenre. """
+    connection = lite.connect('cpcp.db')
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT COUNT(DISTINCT MOVIES.OCLC_ID) FROM MOVIES \
+        WHERE MOVIES.Genre1 = '" + genre + "' OR MOVIES.Genre2 = '" + genre \
+        + "' OR MOVIES.Genre3 = '" + genre + "'")
+        data = cursor.fetchall()
+        return data[0][0]
+    
+def percentageOfOccurrenceByGenre(keywordOrPhrase, genre):
+    """ Returns the percentages of movies containing the keyword/phrase for each
+    movie genre that occurs in the database. """
+    count = occurrencesByGenre(keywordOrPhrase, genre)
+    if totalMoviesOfSpecifiedGenre(genre) != 0:
+        percentage = 100 * count / totalMoviesOfSpecifiedGenre(genre)
+    else:
+        percentage = 0.0
+    return [(genre, percentage)]
+
+print(percentageOfOccurrenceByGenre("cell phone", "Thriller"))
