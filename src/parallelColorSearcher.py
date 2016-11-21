74888c6f145a06bcebe49ccdff023cf928fc5372
Justin Eyster <jde012@bucknell.edu>
2016-11-21 15:20:05 -0500

    Created files parallelColorSearcher.py and singleParallelColorSearch.py
    parallelColorSearcher.py imitates colorSearcher.py but introduces multiprocessing (not multi-threading due to its limitations in python). singleParallelColorSearch.py is exactly the same as singleColorSearch.py except that it uses the parallelColorSearcher instead of the regular one.
    
    Note that this new functionallity has not yet been tested. I lack the external hard drive with the screenshots, else I would test it myself. You'll want to adjust the number of pooled processes and do runtime tests to find the optimal number of processes.

---
+++ src/parallelColorSearcher.py
@@ -0,0 +1,57 @@
+__author__ = "Dale Hartman"
+__date__ = "July 21, 2016 3:38:40 PM$"
+
+import numpy as np
+from multiprocessing import Pool
+
+class ColorSearcher:
+    def __init__(self, index, sorting):
+        # store the index we are reading through
+        self.index = index
+        # store boolean telling us if we should sort the results
+        self.sorting = sorting
+
+    def search(self, queryFeatures):
+        """
+        Take the descriptor of our search image and compute
+        the best matching images from the index
+        """
+
+        # initialize the dictionary of results
+        results = {}
+
+        # loop over the index
+        for (k, features) in self.index.items():
+            # compute the chi-squared distance between the features
+            # in our index and our query features
+            d = self.chi2_distance(features, queryFeatures)
+
+            # now update the results dictionary with that result
+            results[k] = d
+
+        # return the results
+        return results
+
+    def parallelSearch(self, queryFeatures):
+
+        # create a pool of n processes to parallelize the search across
+        # (n=5, to test. can try different n values and compare runtime)
+        p = Pool(5);
+        # map the search function across the queryFeatures in parallel
+        results = p.map(self.search, queryFeatures)
+
+        # sort the results, so that the more relevant results
+        # (smaller numbers) are at the front of the list
+        if self.sorting == True:
+            results = sorted([(v, k) for (k, v) in results.items()])
+
+        # return the results
+        return results
+
+    def chi2_distance(self, histA, histB, eps = 1e-10):
+        # compute the chi-squared distance
+        d = 0.5 * np.sum([((a-b) ** 2) / (a + b + eps)
+            for (a, b) in zip(histA, histB)])
+
+        # return the chi-squared distance
+        return d
