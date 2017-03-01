b31f2f76d33f8b632a601579047d65fc3557721a
nn004@bucknell.edu <nn004@bucknell.edu>
2017-03-01 17:26:25 -0500

    button bug fixes, allow use of npm start to build bundle

--- src/static/js/components/results/ContextDialog.jsx
+++ src/static/js/components/results/ContextDialog.jsx
@@ -18,8 +18,8 @@ SVGLine.propTypes = {
 
 
 const LeftArrow = (props) => (
-    <IconButton>
-        <SvgIcon onClick={props.onClick}>
+    <IconButton onClick={props.onClick}>
+        <SvgIcon>
             <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
             <path d="M0-.5h24v24H0z" fill="none"/>
         </SvgIcon>
@@ -27,24 +27,22 @@ const LeftArrow = (props) => (
 );
 
 const RightArrow = (props) => (
-    <IconButton>
-        <SvgIcon onClick={props.onClick}>
+    <IconButton onClick={props.onClick}>
+        <SvgIcon>
             <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
             <path d="M0-.25h24v24H0z" fill="none"/>
         </SvgIcon>
     </IconButton>
 );
 
-const CloseButton = (props) => (
-    <IconButton>
-        <SvgIcon onClick={props.onClick}>
-            <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24">
-                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-                <path d="M0 0h24v24H0z" fill="none"/>
-            </svg>
-        </SvgIcon>
-    </IconButton>
-);
+// const CloseButton = (props) => (
+//     <IconButton onClick={props.onClick}>
+//         <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24">
+//             <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+//             <path d="M0 0h24v24H0z" fill="none"/>
+//         </svg>
+//     </IconButton>
+// );
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class ContextDialog extends React.Component {
@@ -135,6 +133,7 @@ export default class ContextDialog extends React.Component {
                 slideTo={this.svgTest.bind(this)}
                 index={result.movieLineNumber-1}
                 x={Math.ceil((result.movieLineNumber-1)/this.props.currentFilm.totalNumberOfLines*TIME_LINE_LENGTH)}
+                key={`screenshot${result.movieLineNumber}`}
                 y1="30"
                 y2="65"
                 stroke={"gray"}
@@ -151,7 +150,6 @@ export default class ContextDialog extends React.Component {
             <FullscreenDialog
                 title={'Context'}
                 open={this.state.open}
-                closeIcon={<CloseButton />}
                 onRequestClose={this.handleClose}
             >
 
