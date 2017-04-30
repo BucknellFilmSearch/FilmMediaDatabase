eed5607a2084c272fa5f031b1e66a5ae3f01156b
nn004@bucknell.edu <nn004@bucknell.edu>
2017-04-30 14:58:05 -0400

    extract constants

--- src/static/js/components/search/TextInputModal.jsx
+++ src/static/js/components/search/TextInputModal.jsx
@@ -1,31 +1,12 @@
 import * as React from 'react';
-import { Link } from "react-router";
 import FlatButton from 'material-ui/FlatButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 import {connect} from 'react-redux';
 import {hashHistory} from 'react-router';
-import {createSelector} from 'reselect';
 import Dialog from 'material-ui/Dialog';
-
-const GENRES = ["All", "Action", "Thriller", "Comedy", "Family", "Adventure", "Mystery", "Romance", "Sci-Fi", "Horror",
-    "Drama", "Biography", "Fantasy", "Crime", "War", "Animation", "History", "Musical"];
-
-const STOP_WORDS = ["a", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren\'t", "as", "at",
-    "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can\'t", "cannot",
-    "could", "couldn\'t", "did", "didn\'t", "do", "does", "doesn\'t", "doing", "don\'t", "down", "during", "each",
-    "few", "for", "from", "further", "had", "hadn\'t", "has", "hasn\'t", "have", "haven\'t", "having", "he",
-    "he\'d", "he\'ll", "he\'s", "her", "here", "here\'s", "hers", "herself", "him", "himself", "his", "how",
-    "how\'s", "i", "i\'d", "i\'ll", "i\'m", "i\'ve", "if", "in", "into", "is", "isn\'t", "it", "it\'s", "its",
-    "itself", "let\'s", "me", "more", "most", "mustn\'t", "my", "myself", "no", "nor", "not", "of", "off", "on",
-    "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan\'t",
-    "she", "she\'d", "she\'ll", "she\'s", "should", "shouldn\'t", "so", "some", "such", "than", "that", "that\'s",
-    "the", "their", "theirs", "them", "themselves", "then", "there", "there\'s", "these", "they", "they\'d",
-    "they\'ll", "they\'re", "they\'ve", "this", "those", "through", "to", "too", "under", "until", "up", "very",
-    "was", "wasn\'t", "we", "we\'d", "we\'ll", "we\'re", "we\'ve", "were", "weren\'t", "what", "what\'s", "when",
-    "when\'s", "where", "where\'s", "which", "while", "who", "who\'s", "whom", "why", "why\'s", "with", "won\'t",
-    "would", "wouldn\'t", "you", "you\'d", "you\'ll", "you\'re", "you\'ve", "your", "yours", "yourself", "yourselves"];
+import {STOP_WORDS, GENRES} from '../helpers';
 
 const modalStyle = {
     textAlign: 'center'
