cc1384464f5a8e723add416816117b2ae9bbf0aa
nn004@bucknell.edu <nn004@bucknell.edu>
2017-04-30 14:35:38 -0400

    add documentation for app.jsx

--- src/static/js/app.jsx
+++ src/static/js/app.jsx
@@ -1,31 +1,37 @@
-// Library imports
-import injectTapEventPlugin from 'react-tap-event-plugin'; // temporary dependency for material-ui
+/**
+ * This file is used to instantiate the application. This includes creating the Redux store,
+ * defining the routes for React-Router, and rendering components to the DOM.
+ */
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import React from 'react';
 import ReactDOM from "react-dom";
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
-
-// React component imports
 import SearchContainer from "./components/search/SearchContainer.jsx";
 import AllFilms from "./components/results/AllFilms.jsx";
+import 'isomorphic-fetch';
 
+
+// allows use of chrome extension to monitor react performance
 import Perf from 'react-addons-perf';
-
 window.Perf = Perf;
 
-
-// Global settings
+// can be used to replace images with placeholder images to save bandwidth
 export let DEBUG_MODE = true;
 
 injectTapEventPlugin();
 
-import 'isomorphic-fetch';
 
-
-
+/**
+ * Generates the next state of the application. Changes are passed to the components for rerendering.
+ *
+ * @param state The previous state of the application
+ * @param action An action triggered by the user or an API call
+ * @return {*} The next state of the application
+ */
 export const reducer = (state = {search: null, context: [], sortType: 1, genre: 'All'}, action) => {
     switch (action.type) {
         case 'MOUSE_ENTER_SCREENSHOT':
@@ -139,6 +145,8 @@ export const reducer = (state = {search: null, context: [], sortType: 1, genre: 
     }
 };
 
+
+// create the Redux store
 const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
@@ -146,6 +154,7 @@ const store = createStore(
 );
 
 
+// render the components to the page and define the routes on the page
 ReactDOM.render((
     <Provider store={store}>
         <Router history={hashHistory}>
