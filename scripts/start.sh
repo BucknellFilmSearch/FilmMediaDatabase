33f2a164607e070c21b2844467007b5193517efc
Eric Marshall <emm035@bucknell.edu>
2017-12-11 22:48:53 -0500

    Added coloring to startup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-trap 'kill %1' SIGINT
+colorize() {
+    echo -e "$(tput setaf $2)$1$(tput sgr0)";
+}
+
+log() {
+    while read line; do
+        echo "$(colorize "[$1]" $2) $line";
+    done;
+}
+
+pythonLogClr=3
+nodeLogClr=1
+
+trap 'kill $(jobs -p) &> /dev/null' SIGINT
 
 # This script starts up the development session on localhost:8080
 pushd $(dirname $0) &> /dev/null
@@ -9,15 +22,19 @@ pushd .. &> /dev/null
 mkdir -p logs
 logdir=$(pwd)/logs
 
+colorize 'Starting Session' 2
+
 pushd src &> /dev/null
-python main.py 2>&1 | tee ${logdir}/python.log | sed -e 's/^/[Python] /' &
+python main.py 2>&1 | tee ${logdir}/python.log | log 'python' 3 &
 
 pushd static/js &> /dev/null
-npm start 2>&1 | tee $logdir/node.log | sed -e 's/^/[Node]   /'
+npm start 2>&1 | tee $logdir/node.log | log 'node' 1
 
 popd &> /dev/null
 popd &> /dev/null
 popd &> /dev/null
 popd &> /dev/null
-echo "Session successfully started"
+
+colorize '\nSession successfully stopped' 2
+
 exit 0
