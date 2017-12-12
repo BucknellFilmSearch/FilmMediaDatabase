db86020a071144e5a7859870fcf569f27303a4ba
Eric Marshall <emm035@bucknell.edu>
2017-12-12 11:13:09 -0500

    Improved setup script
    - Checks for yarn (and use that if found)
    - Checks for a virtual environment & asks for confirmation if not found
    - More robust checks for whether files exist

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,24 +7,47 @@ set -e
 pushd $(dirname $0) >> /dev/null
 pushd .. >> /dev/null
 
-which pip2
-if [[ $? == 1 ]]; then
-  echo "Please install pip and then run this script again..."
+type pip2 &> /dev/null
+if [[ $? != 0 ]]; then
+  echo "Please install https://pip.pypa.io/en/stable/ and then run this script again..."
   exit 1
 fi
 
-which npm
-if [[ $? == 1 ]]; then
-  echo "Please install npm and then run this script again..."
-  exit 1
+# Check for either npm or yarn
+type npm &> /dev/null
+if [[ $? != 0 ]]; then
+  type yarn &> /dev/null
+  if [[ $? != 0 ]]; then
+    echo "Please install https://yarnpkg.com/en/ or https://www.npmjs.com/ and then run this script again..."
+    exit 1
+  fi
+fi
+
+# Check if in a virtual environment - if not, confirm running
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo 
+    read -r -p "It looks like you're not in a python virtual environment, continue? [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            ;;
+        *)
+            exit 0
+            ;;
+    esac
 fi
 
 # install pip requirments
 pushd src >> /dev/null
 pip install -r requirements.txt
 
+# Install js libs with yarn if it's found, otherwise with npm
 pushd static/js >> /dev/null
-npm install
+type yarn &> /dev/null
+if [[ $? == 0 ]]; then
+  yarn install
+else
+  npm install
+fi
 
 # success
 popd >> /dev/null
