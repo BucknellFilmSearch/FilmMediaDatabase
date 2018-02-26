cd1c43204e1c5dd41811beab379399c1e7b5209d
Cole Whitley <colewhit29@gmail.com>
2018-02-26 17:09:07 -0500

    updated files, working on making bounding boxes show and go away

--- src/server/src/static/js/components/results/ScreenshotWithCaption.jsx
+++ src/server/src/static/js/components/results/ScreenshotWithCaption.jsx
@@ -36,7 +36,7 @@ export default class ScreenshotWithCaption extends React.Component {
 
         let imgSrc =
             "http://www.filmtvsearch.net/static/imageFiles/screenshots/" + this.props.movieOclcId + "/" + this.props.screenshotDetails.movieLineNumber + ".png";
-        
+
         return (
             <GridTile
                 style={{'height': '180px', objectFit:'fill'}}
