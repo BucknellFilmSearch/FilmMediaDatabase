0d0edb529c52b5be30da74283807a3f9ba46356a
Eric Marshall <emm035@bucknell.edu>
2018-03-29 15:45:41 -0400

    Images are now fetched from the S3 Bucket

--- src/server/src/static/js/components/results/ScreenshotWithCaption.jsx
+++ src/server/src/static/js/components/results/ScreenshotWithCaption.jsx
@@ -34,7 +34,7 @@ export default class ScreenshotWithCaption extends React.Component {
      */
   render() {
 
-    let imgSrc = `http://www.filmtvsearch.net/static/imageFiles/screenshots/${this.props.movieOclcId}/${this.props.screenshotDetails.movieLineNumber}.png`;
+    let imgSrc = `${process.env.IMG_SRC}${this.props.movieOclcId}/${this.props.screenshotDetails.movieLineNumber}.png`;
 
     return (
       <GridTile
